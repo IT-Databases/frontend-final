ca69fe6eab004434a263274dd17cc41b52a33baa
Muhammad Arfani Asra <arfani152@gmail.com>
2024-02-26 12:07:40 +0800

    Initial Commit: Landing page final Yamali CLP 7

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,9 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: ["./*.html"],
+  content: ["./*.html",
+      "./node_modules/tw-elements/dist/js/**/*.js",],
+
   theme: {
     fontFamily: {
       jost: "Jost, sans-serif",
@@ -28,9 +30,10 @@ module.exports = {
         "red-bata": "hsl(7,78%,60%)",
       },
       backgroundImage: {
-        "hero-header": "url('../img/bg/header.png')",
-        "hero-topics": "url('../img/bg/topics.png')",
-        "hero-committees": "url('../img/bg/committe.png')",
+        "hero-header": "url('../img/bg/Hero-Bg.jpeg')",
+        "hero-topics": "url('../img/bg/theme.jpeg')",
+        "hero-bg": "url('../img/bg/theme.jpeg')",
+        "hero-committees": "url('../img/bg/theme.jpeg')",
       },
       gridTemplateColumns: {
         "max-2": "repeat(2,max-content)",
@@ -38,5 +41,6 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [require("tw-elements/dist/plugin.cjs")],
+  darkMode: "class"
 };
